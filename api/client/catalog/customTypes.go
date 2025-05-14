14884dd08a861655e89d2d88243c83aec7705552
Jayc <mjayachand@gmail.com>
2025-05-14 14:48:47 +0530

    chore: add custom type to api client

---
+++ api/client/catalog/customTypes.go
@@ -0,0 +1,92 @@
+package catalog
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type CustomTypeCreate struct {
+	Name        string                 `json:"name"`
+	Description string                 `json:"description"`
+	Type        string                 `json:"type"`
+	Config      map[string]interface{} `json:"config,omitempty"`
+	Properties  []CustomTypeProperty   `json:"properties,omitempty"`
+}
+
+type CustomType struct {
+	ID          string                 `json:"id"`
+	Name        string                 `json:"name"`
+	Version     int                    `json:"version"`
+	Description string                 `json:"description"`
+	Type        string                 `json:"type"`
+	DataType    string                 `json:"dataType"`
+	WorkspaceId string                 `json:"workspaceId"`
+	Config      map[string]interface{} `json:"config"`
+	Rules       map[string]interface{}      `json:"rules"`
+	Properties  []CustomTypeProperty   `json:"properties"`
+	ItemDefinitions []string             `json:"itemDefinitions"`
+	CreatedAt   time.Time              `json:"createdAt"`
+	UpdatedAt   time.Time              `json:"updatedAt"`
+	CreatedBy   string                 `json:"createdBy"`
+	UpdatedBy   string                 `json:"updatedBy"`
+}
+
+type CustomTypeProperty struct {
+	ID        string                 `json:"id"`
+	Required  bool                   `json:"required"`
+}
+
+type CustomTypeStore interface {
+	CreateCustomType(ctx context.Context, input CustomTypeCreate) (*CustomType, error)
+	UpdateCustomType(ctx context.Context, id string, input *CustomType) (*CustomType, error)
+	DeleteCustomType(ctx context.Context, id string) error
+}
+
+func (c *RudderDataCatalog) DeleteCustomType(ctx context.Context, id string) error {
+	_, err := c.client.Do(ctx, "DELETE", fmt.Sprintf("catalog/custom-types/%s", id), nil)
+	if err != nil {
+		return fmt.Errorf("sending request: %w", err)
+	}
+	return nil
+}
+
+func (c *RudderDataCatalog) UpdateCustomType(ctx context.Context, id string, new *CustomType) (*CustomType, error) {
+	byt, err := json.Marshal(new)
+	if err != nil {
+		return nil, fmt.Errorf("marshalling input: %w", err)
+	}
+
+	resp, err := c.client.Do(ctx, "PUT", fmt.Sprintf("catalog/custom-types/%s", id), bytes.NewReader(byt))
+	if err != nil {
+		return nil, fmt.Errorf("sending request: %w", err)
+	}
+
+	var customType CustomType
+	if err := json.NewDecoder(bytes.NewReader(resp)).Decode(&customType); err != nil {
+		return nil, fmt.Errorf("decoding response: %w", err)
+	}
+
+	return &customType, nil
+}
+
+func (c *RudderDataCatalog) CreateCustomType(ctx context.Context, input CustomTypeCreate) (*CustomType, error) {
+	byt, err := json.Marshal(input)
+	if err != nil {
+		return nil, fmt.Errorf("marshalling input: %w", err)
+	}
+
+	resp, err := c.client.Do(ctx, "POST", "catalog/custom-types", bytes.NewReader(byt))
+	if err != nil {
+		return nil, fmt.Errorf("executing http request: %w", err)	
+	}
+
+	customType := CustomType{} // Create a holder for response object
+	if err := json.NewDecoder(bytes.NewReader(resp)).Decode(&customType); err != nil {
+		return nil, fmt.Errorf("decoding response: %w", err)
+	}
+
+	return &customType, nil
+}
