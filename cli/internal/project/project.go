f3930d20b00869fbd876f41fc02aa2e16990f679
Fotis Xenikoudakis <fotis@rudderstack.com>
2025-05-26 13:13:31 +0300

    test: added project tests

--- cli/internal/project/project.go
+++ cli/internal/project/project.go
@@ -2,12 +2,19 @@ package project
 
 import (
 	"context"
+	"fmt"
 
-	"github.com/rudderlabs/rudder-iac/cli/internal/project/loader"
+	"github.com/rudderlabs/rudder-iac/cli/internal/project/loader" // Will be used for default loader
 	"github.com/rudderlabs/rudder-iac/cli/internal/project/specs"
 	"github.com/rudderlabs/rudder-iac/cli/internal/syncer/resources"
 	"github.com/rudderlabs/rudder-iac/cli/internal/syncer/state"
 )
+
+// SpecLoader defines the interface for loading project specifications.
+type Loader interface {
+	// Load loads specifications from the specified location.
+	Load(location string) (map[string]*specs.Spec, error)
+}
 
 type ProjectProvider interface {
 	GetSupportedKinds() []string
@@ -39,40 +46,62 @@ type Project interface {
 type project struct {
 	Location string
 	Provider Provider
+	loader   Loader // New field
+	// specs      []*specs.Spec // This seems to be handled by the provider internally via LoadSpec
 }
 
-func New(location string, provider Provider) Project {
-	return &project{
+// ProjectOption defines a functional option for configuring a Project.
+type ProjectOption func(*project)
+
+// WithSpecLoader allows providing a custom SpecLoader.
+func WithLoader(l Loader) ProjectOption {
+	return func(p *project) {
+		if l != nil {
+			p.loader = l
+		}
+	}
+}
+
+// New creates a new Project instance.
+// By default, it uses a loader.Loader.
+func New(location string, provider Provider, opts ...ProjectOption) Project {
+	p := &project{
 		Location: location,
 		Provider: provider,
 	}
+
+	for _, opt := range opts {
+		opt(p)
+	}
+
+	if p.loader == nil {
+		p.loader = &loader.Loader{}
+	}
+
+	return p
 }
 
+// Load loads the project specifications using the configured SpecLoader
+// and then validates them with the provider.
 func (p *project) Load() error {
-	if err := p.loadSpecs(p.Location); err != nil {
-		return err
-	}
-	return p.Provider.Validate()
-}
-
-func (p *project) loadSpecs(location string) error {
-	l := loader.New(location)
-	s, err := l.Load()
+	loadedSpecs, err := p.loader.Load(p.Location) // Use the specLoader
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to load specs using specLoader: %w", err)
 	}
 
-	for path, spec := range s {
+	// The rest of the logic from old loadSpecs
+	for path, spec := range loadedSpecs {
 		if !p.providerSupportsKind(spec.Kind) {
 			return specs.ErrUnsupportedKind{
 				Kind: spec.Kind,
 			}
 		}
 		if err := p.Provider.LoadSpec(path, spec); err != nil {
-			return err
+			return fmt.Errorf("provider failed to load spec from path %s: %w", path, err)
 		}
 	}
-	return nil
+
+	return p.Provider.Validate()
 }
 
 func (p *project) providerSupportsKind(kind string) bool {
