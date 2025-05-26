56ca46a57f7001245d1bb50c9378aa6d6f2e93e4
Fotis Xenikoudakis <fotis@rudderstack.com>
2025-05-26 10:47:47 +0300

    test: added loader tests

--- cli/internal/project/loader/loader.go
+++ cli/internal/project/loader/loader.go
@@ -12,11 +12,16 @@ import (
 
 var log = logger.New("loader")
 
+// Loader is responsible for finding and loading project specification files.
 type Loader struct {
 	// Location is the root directory of the project
 	Location string
 }
 
+// New creates and returns a new Loader instance, with a target location for spec files.
+// If no location is provided, it defaults to the current directory ".".
+// This function is not loading any specs or checking the existince of directories,
+// it only initializes the Loader with a location.
 func New(location string) *Loader {
 	if location == "" {
 		location = "."
@@ -27,6 +32,10 @@ func New(location string) *Loader {
 	}
 }
 
+// Load scans the configured location for YAML files (.yaml or .yml).
+// It walks the directory tree recursively to discover them, and parses them into Spec objects.
+// It returns a map of file paths to their corresponding Spec objects,
+// or an error if any file operation or spec parsing fails.
 func (l *Loader) Load() (map[string]*specs.Spec, error) {
 	var allSpecs map[string]*specs.Spec = make(map[string]*specs.Spec)
 
