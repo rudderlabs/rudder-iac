df917d62d0d72437fc0dd0edcb564d6fb1254e3c
Abhimanyu Babbar <abhimanyu.babbar89@gmail.com>
2025-06-19 13:45:25 +0530

    chore: add structure for the e2e test for trackingplan apply

---
+++ cli/tests/command_apply_test.go
@@ -0,0 +1,21 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTrackingPlanApply(t *testing.T) {
+	t.Parallel()
+
+	t.Run("should create entities in catalog from project", func(t *testing.T) {
+		t.Parallel()
+		require.True(t, true)
+	})
+
+	t.Run("should update entities in catalog from project", func(t *testing.T) {
+		t.Parallel()
+		require.True(t, true)
+	})
+}
