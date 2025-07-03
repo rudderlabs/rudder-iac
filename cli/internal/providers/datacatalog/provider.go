9bca3edfa58145a38b40a1234a659d82ad769d09
Dhawal Sanghvi <sanghvi.dhawal101433@gmail.com>
2025-07-03 14:28:36 +0530

    chore: move validate package to datacatalog provider

--- cli/internal/providers/datacatalog/provider.go
+++ cli/internal/providers/datacatalog/provider.go
@@ -8,8 +8,8 @@ import (
 	"github.com/rudderlabs/rudder-iac/cli/internal/logger"
 	"github.com/rudderlabs/rudder-iac/cli/internal/project/specs"
 	pstate "github.com/rudderlabs/rudder-iac/cli/internal/providers/datacatalog/state"
+	"github.com/rudderlabs/rudder-iac/cli/internal/providers/datacatalog/validate"
 	"github.com/rudderlabs/rudder-iac/cli/internal/syncer/resources"
-	"github.com/rudderlabs/rudder-iac/cli/internal/validate"
 	"github.com/rudderlabs/rudder-iac/cli/pkg/localcatalog"
 )
 
