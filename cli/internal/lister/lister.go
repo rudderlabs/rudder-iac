c54b01f6d95c5dd322c44dcaba3ba989972430db
Fotis Xenikoudakis <fotis@rudderstack.com>
2025-07-04 14:04:25 +0300

    feat: implemnented basic table formatting for list outputs

--- cli/internal/lister/lister.go
+++ cli/internal/lister/lister.go
@@ -10,26 +10,52 @@ import (
 	"github.com/rudderlabs/rudder-iac/cli/internal/ui"
 )
 
+// OutputFormat determines how the lister should format its output.
+type OutputFormat string
+
+const (
+	// JSONFormat outputs each resource as a JSON object on a new line.
+	JSONFormat OutputFormat = "json"
+	// TableFormat outputs resources in a human-readable table.
+	TableFormat OutputFormat = "table"
+)
+
 // Filters represents a generic way to pass key-value filter criteria.
 type Filters map[string]string
 
 type Lister struct {
 	Provider project.Provider
+	Format   OutputFormat
 }
 
 func (l *Lister) List(ctx context.Context, resourceType string, filters Filters) error {
-	spinner := ui.NewSpinner(fmt.Sprintf("Fetching %s...", resourceType))
-	spinner.Start()
-	resources, err := l.Provider.List(ctx, resourceType, filters)
-	spinner.Stop()
+	var rs []resources.ResourceData
+	var err error
+
+	if l.Format == TableFormat {
+		spinner := ui.NewSpinner(fmt.Sprintf("Fetching %s...", resourceType))
+		spinner.Start()
+		rs, err = l.Provider.List(ctx, resourceType, filters)
+		spinner.Stop()
+	} else {
+		rs, err = l.Provider.List(ctx, resourceType, filters)
+	}
+
 	if err != nil {
 		return err
 	}
 
-	return printResources(resources)
+	switch l.Format {
+	case JSONFormat:
+		return printResourcesAsJSON(rs)
+	case TableFormat:
+		return printResourcesAsTable(rs)
+	default:
+		return fmt.Errorf("unknown output format: %s", l.Format)
+	}
 }
 
-func printResources(resources []resources.ResourceData) error {
+func printResourcesAsJSON(resources []resources.ResourceData) error {
 	for _, r := range resources {
 		b, err := json.Marshal(r)
 		if err != nil {
@@ -40,8 +66,9 @@ func printResources(resources []resources.ResourceData) error {
 	return nil
 }
 
-func New(p project.Provider) *Lister {
+func New(p project.Provider, format OutputFormat) *Lister {
 	return &Lister{
 		Provider: p,
+		Format:   format,
 	}
 }
