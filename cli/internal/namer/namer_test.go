9f40747ddfae62e731af73433f3bd3f9b3c586b8
Abhimanyu Babbar <abhimanyu.babbar89@gmail.com>
2025-09-17 13:34:11 +0530

    chore: add new namer package to create a naming abstraction for externalId naming (#171)
    ## Description of the change
    
    ### Overview
    We are introducing a new abstraction to provide ability to name
    `externalId` uniquely during the import flow in the cli. This is done
    using a namer package based on the `*core.NameRegistry`.
    
    The namer abstraction is as follows:
    ```
    type Namer interface {
    	Name(input string) (string, error)
    	Load([]string) error
    }
    ```
    
    * `Load` function is used to prepopulate the namer with existing
    externalIds
    * `Name` can then be called to get a unique name based on predefined
    strategy. In this case, we are using `KebabCase` as a default and
    predefined strategy to populate the names.
    
    ### Usage
    
    ```
    n := NewExternalIdNamer(NewKebabCase())
    n.Load([]string{"api-requested", "api-responded"})
    n.Name("api-requested") // -> "api-requested-1"
    ```
    
    ### Concurrent Safe
    The functionality to Load and Name is concurrent safe achieved through a
    `sync.Mutex`
    
    
    ## Type of change
    - [ ] Bug fix (non-breaking change that fixes an issue)
    - [ ] New feature (non-breaking change that adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    
    ## Related issues
    
    Fixes: **DAW-2125**
    https://linear.app/rudderstack/issue/DAW-2125/3-namer-interface
    
    ## Checklists
    
    ### Development
    
    - [ ] Lint rules pass locally
    - [ ] The code changed/added as part of this pull request has been
    covered with tests
    - [ ] All tests related to the changed code pass in development
    
    ### Code review 
    
    - [ ] This pull request has a descriptive title and information useful
    to a reviewer. There may be a screenshot or screencast attached
    - [ ] "Ready for review" label attached to the PR and reviewers
    mentioned in a comment
    - [ ] Changes have been reviewed by at least one other engineer
    - [ ] Issue from task tracker has a link to this pull request

---
+++ cli/internal/namer/namer_test.go
@@ -0,0 +1,114 @@
+package namer
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestKebabCase_Name(t *testing.T) {
+	t.Parallel()
+	strategy := NewKebabCase()
+
+	tests := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{"simple phrase", "Hello World", "hello-world"},
+		{"with numbers", "User123 Signed Up", "user123-signed-up"},
+		{"special chars", "User@Signed#Up", "user-signed-up"},
+		{"multiple spaces", "Hello   World", "hello-world"},
+		{"leading trailing", " Hello World ", "hello-world"},
+		{"empty", "", ""},
+		{"single word", "Hello", "hello"},
+		{"all special", "@#$%", ""},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			result := strategy.Name(tt.input)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
+
+func TestExternalIdNamer_Name(t *testing.T) {
+	t.Parallel()
+	n := NewExternalIdNamer(NewKebabCase())
+
+	tests := []struct {
+		name     string
+		inputs   []string
+		expected []string
+	}{
+		{"unique names", []string{"HelloWorld", "Another Event"}, []string{"helloworld", "another-event"}},
+		{"collisions", []string{"Hello World", "Hello World", "Hello World"}, []string{"hello-world", "hello-world-1", "hello-world-2"}},
+		{"extra collisions", []string{"Test", "Test", "Test", "Test"}, []string{"test", "test-1", "test-2", "test-3"}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			for i, input := range tt.inputs {
+				result, err := n.Name(input)
+				assert.NoError(t, err)
+				assert.Equal(t, tt.expected[i], result)
+			}
+		})
+	}
+}
+
+func TestExternalIdNamer_Load(t *testing.T) {
+	t.Parallel()
+	n := NewExternalIdNamer(NewKebabCase())
+
+	tests := []struct {
+		name    string
+		names   []string
+		wantErr bool
+		errMsg  string
+	}{
+		{"empty", []string{}, false, ""},
+		{"no duplicates", []string{"three", "four"}, false, ""},
+		{"with duplicates", []string{"one", "one"}, true, "loading name: one errored with: duplicate name exception"},
+		{"extra duplicates", []string{"test", "test", "test"}, true, "loading name: test errored with: duplicate name exception"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			err := n.Load(tt.names)
+			if tt.wantErr {
+				assert.Error(t, err)
+				assert.Contains(t, err.Error(), tt.errMsg)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestCollisionHandler(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		base     string
+		existing []string
+		expected string
+	}{
+		{"simple collision", "base", []string{"base"}, "base-1"},
+		{"multiple collisions", "base", []string{"base", "base-1", "base-2"}, "base-3"},
+		{"edge empty", "", []string{}, "-1"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			result := collisionHandler(tt.base, tt.existing)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
