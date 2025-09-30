063adc948c28e4d42af738fcc16f00639ae77095
Shashank Shekhar <shekhar@rudderstack.com>
2025-09-30 15:17:54 +0530

    feat: add support for event stream source governance validations

--- api/client/event-stream/source/types.go
+++ api/client/event-stream/source/types.go
@@ -1,5 +1,7 @@
 package source
 
+import trackingplanClient "github.com/rudderlabs/rudder-iac/api/client/event-stream/tracking-plan-connection"
+
 type CreateSourceRequest struct {
 	ExternalID string `json:"externalId"`
 	Name       string `json:"name"`
@@ -18,6 +20,12 @@ type EventStreamSource struct {
 	Name        string `json:"name"`
 	Type        string `json:"type"`
 	Enabled     bool   `json:"enabled"`
+	TrackingPlan *TrackingPlan `json:"trackingPlan"`
+}
+
+type TrackingPlan struct {
+	ID string `json:"id"`
+	Config *trackingplanClient.ConnectionConfig `json:"config"`
 }
 
 type eventStreamSources struct {
