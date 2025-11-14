046833361698633ef3f0258eb39633bfaf08850d
Abhimanyu Babbar <abhimanyu.babbar89@gmail.com>
2025-11-14 11:02:53 +0530

    feat: use rebuildSchemas=false to prevent brittleness when fetching the trackingplan event identifiers
    🔒 Scanned for secrets using gitleaks 8.29.0

--- api/client/catalog/trackingplans.go
+++ api/client/catalog/trackingplans.go
@@ -393,7 +393,12 @@ func (c *RudderDataCatalog) GetTrackingPlans(ctx context.Context, options ListOp
 }
 
 func (c *RudderDataCatalog) GetTrackingPlanEventWithIdentifiers(ctx context.Context, id, eventId string) (*TrackingPlanEventPropertyIdentifiers, error) {
-	resp, err := c.client.Do(ctx, "GET", fmt.Sprintf("v2/catalog/tracking-plans/%s/events/%s?format=properties", id, eventId), nil)
+	resp, err := c.client.Do(
+		ctx,
+		"GET",
+		fmt.Sprintf("v2/catalog/tracking-plans/%s/events/%s?format=properties&rebuildSchemas=false", id, eventId),
+		nil,
+	)
 	if err != nil {
 		return nil, fmt.Errorf("executing http request: %w", err)
 	}
