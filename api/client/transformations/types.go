5d6442962e2ca451e4f6e4ae2cb7f305680c0aab
Akash Gupta <akashguptarpo@gmail.com>
2026-01-15 17:21:59 +0530

    feat: refactor 2
    🔒 Scanned for secrets using gitleaks 8.28.0

--- api/client/transformations/types.go
+++ api/client/transformations/types.go
@@ -26,17 +26,24 @@ type TransformationLibrary struct {
 	ExternalID  string `json:"externalId,omitempty"`
 }
 
-// CreateTransformationRequest is the request body for creating/updating transformations
+// CreateTransformationRequest is the request body for creating a transformation
 type CreateTransformationRequest struct {
 	Name        string  `json:"name"`
 	Description *string `json:"description"`
 	Code        string  `json:"code"`
 	Language    string  `json:"language"`
-	TestEvents  []any   `json:"testEvents,omitempty"`
 	ExternalID  *string `json:"externalId,omitempty"`
 }
 
-// CreateLibraryRequest is the request body for creating/updating libraries
+// UpdateTransformationRequest is the request body for updating a transformation
+type UpdateTransformationRequest struct {
+	Name        string  `json:"name"`
+	Description *string `json:"description"`
+	Code        string  `json:"code"`
+	Language    string  `json:"language"`
+}
+
+// CreateLibraryRequest is the request body for creating a library
 type CreateLibraryRequest struct {
 	Name        string  `json:"name"`
 	Description *string `json:"description"`
@@ -45,22 +52,27 @@ type CreateLibraryRequest struct {
 	ExternalID  *string `json:"externalId,omitempty"`
 }
 
+// UpdateLibraryRequest is the request body for updating a library
+type UpdateLibraryRequest struct {
+	Name        string  `json:"name"`
+	Description *string `json:"description"`
+	Code        string  `json:"code"`
+	Language    string  `json:"language"`
+}
+
 // BatchPublishRequest is the request body for batch publishing transformations and libraries
 type BatchPublishRequest struct {
 	Transformations []BatchPublishTransformation `json:"transformations,omitempty"`
 	Libraries       []BatchPublishLibrary        `json:"libraries,omitempty"`
 }
 
+// BatchPublishTransformation represents a transformation to publish
 type BatchPublishTransformation struct {
 	VersionID string `json:"versionId"`
 	TestInput []any  `json:"testInput,omitempty"`
 }
 
+// BatchPublishLibrary represents a library to publish
 type BatchPublishLibrary struct {
 	VersionID string `json:"versionId"`
-}
-
-// BatchPublishResponse is the response from batch publish
-type BatchPublishResponse struct {
-	Published bool `json:"published"`
 }
