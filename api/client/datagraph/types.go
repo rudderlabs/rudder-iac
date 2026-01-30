a18225448dd656be342da226ad2203c559382734
Fotis Xenikoudakis <fotis@rudderstack.com>
2026-01-30 15:50:25 +0200

    refactor: change client implementation according to update openapi spec
    🔒 Scanned for secrets using gitleaks 8.28.0

--- api/client/datagraph/types.go
+++ api/client/datagraph/types.go
@@ -8,24 +8,18 @@ import (
 
 // DataGraph represents a data graph entity from the API
 type DataGraph struct {
-	ID                 string     `json:"id,omitempty"`
-	Name               string     `json:"name"`
-	WorkspaceID        string     `json:"workspaceId,omitempty"`
-	WarehouseAccountID string     `json:"warehouseAccountId,omitempty"`
-	ExternalID         string     `json:"externalId,omitempty"`
-	CreatedAt          *time.Time `json:"createdAt,omitempty"`
-	UpdatedAt          *time.Time `json:"updatedAt,omitempty"`
+	ID          string     `json:"id,omitempty"`
+	WorkspaceID string     `json:"workspaceId,omitempty"`
+	AccountID   string     `json:"accountId,omitempty"`
+	ExternalID  string     `json:"externalId,omitempty"`
+	CreatedAt   *time.Time `json:"createdAt,omitempty"`
+	UpdatedAt   *time.Time `json:"updatedAt,omitempty"`
 }
 
 // CreateDataGraphRequest is the request body for creating a data graph
 type CreateDataGraphRequest struct {
-	Name               string `json:"name"`
-	WarehouseAccountID string `json:"warehouseAccountId"`
-}
-
-// UpdateDataGraphRequest is the request body for updating a data graph
-type UpdateDataGraphRequest struct {
-	Name string `json:"name"`
+	AccountID  string `json:"accountId"`
+	ExternalID string `json:"externalId,omitempty"`
 }
 
 // ListDataGraphsResponse represents the paginated response from listing data graphs
