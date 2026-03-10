dbea61d3c3a4e84134a432fbba8a7b9fd82da7de
Jayc <mjayachand@gmail.com>
2026-03-11 00:22:23 +0530

    chore: add python transform e2e tests on apply
    🔒 Scanned for secrets using gitleaks 8.28.0

---
+++ cli/tests/testdata/project/create/transformations/python/simple_python_transform.py
@@ -0,0 +1,9 @@
+def transformEvent(event, metadata):
+    if 'properties' not in event:
+        event['properties'] = {}
+
+    event['properties']['message'] = 'Hello from Python!'
+    if 'userId' in event:
+        event['properties']['formatted_user'] = f"User: {event['userId']}"
+
+    return event
