19b16b434760b53bda444fc7520e51df748af6d2
Abhimanyu Babbar <abhimanyu.babbar89@gmail.com>
2026-08-26 22:10:28 +0530

    feat(destination): onboard googleads CLI definition (#697)

--- cli/internal/app/dependencies.go
+++ cli/internal/app/dependencies.go
@@ -33,6 +33,7 @@ import (
 	"github.com/rudderlabs/rudder-iac/cli/internal/providers/destination/definitions/ga"
 	"github.com/rudderlabs/rudder-iac/cli/internal/providers/destination/definitions/ga4"
 	"github.com/rudderlabs/rudder-iac/cli/internal/providers/destination/definitions/gcs"
+	"github.com/rudderlabs/rudder-iac/cli/internal/providers/destination/definitions/googleads"
 	googlepubsub "github.com/rudderlabs/rudder-iac/cli/internal/providers/destination/definitions/googlepubsub"
 	googlesheets "github.com/rudderlabs/rudder-iac/cli/internal/providers/destination/definitions/googlesheets"
 	"github.com/rudderlabs/rudder-iac/cli/internal/providers/destination/definitions/gtm"
@@ -361,6 +362,9 @@ func newDestinationRegistry(cfg config.Config) (*definitions.Registry, error) {
 		}
 		if err := registry.Register(gcs.NewDefinition()); err != nil {
 			return nil, fmt.Errorf("registering gcs destination definition: %w", err)
+		}
+		if err := registry.Register(googleads.NewDefinition()); err != nil {
+			return nil, fmt.Errorf("registering googleads destination definition: %w", err)
 		}
 		if err := registry.Register(googlepubsub.NewDefinition()); err != nil {
 			return nil, fmt.Errorf("registering googlepubsub destination definition: %w", err)
