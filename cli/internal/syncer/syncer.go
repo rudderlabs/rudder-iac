1c812cfd95d6a4dba895078d4327454827e99696
Dhawal Sanghvi <sanghvi.dhawal101433@gmail.com>
2026-09-24 12:19:16 +0530

    feat(syncer): show a spinner during the consolidate sync

--- cli/internal/syncer/syncer.go
+++ cli/internal/syncer/syncer.go
@@ -171,6 +171,7 @@ func (s *ProjectSyncer) apply(ctx context.Context, target *resources.Graph, cont
 
 	// Consolidate sync: providers can perform batch operations or multi-resource
 	// coordination after all individual resources have been processed
+	ui.StartSpinner("Finalizing ...")
 	if err := s.provider.ConsolidateSync(ctx, target, state); err != nil {
 		return []error{err}
 	}
